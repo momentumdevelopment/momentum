3f5bd49628f5c1cfe3b7b250b6294f55719c0b14
seasnail8169 <seasnail8169@protonmail.com>
2021-03-02 05:20:30 +0000

    embed color :p

--- .circleci/discordwebhook/main.js
+++ .circleci/discordwebhook/main.js
@@ -17,7 +17,7 @@ axios.post(process.env.discord_webhook, {
         {
             title: `Build #${build}`,
             description: description,
-            color: 3066993
+            color: "#3c4ad3"
         }
     ]
 })
