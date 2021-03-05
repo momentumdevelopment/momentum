ee3f5e087e987665434a1d99bb6f198c9dabf1b7
ThatOMA <75460673+YeahOMA@users.noreply.github.com>
2021-03-05 16:01:16 -0600

    Update Momentum.java

--- src/main/java/me/linus/momentum/Momentum.java
+++ src/main/java/me/linus/momentum/Momentum.java
@@ -31,7 +31,7 @@ import org.lwjgl.opengl.Display;
 public class Momentum {
 	
     public static final String MODID = "momentum";
-    public static final String CLIENTNAME = "Momentum";
+    public static final String CLIENTNAME = "MomentumSex";
     public static final String VERSION = "1.2.2-beta";
     public static String NAME = "Momentum";
     public static final Logger LOGGER;
